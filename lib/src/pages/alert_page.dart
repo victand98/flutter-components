eef1093424ad463226ec6eb5be1f3968e213e006
Víctor Andrés Rojas <victandres98@gmail.com>
2021-04-28 17:36:21 -0500

    Alert Component

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,12 +7,51 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Alert Page"),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _showAlert(context),
+          child: Text("Mostrar"),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_left),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+          title: Text("Alert Title"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("This is the Alert's content :D"),
+              FlutterLogo(
+                size: 100.0,
+              )
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Cancelar"),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Ok"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
