11db8c7d1dcbc5e41efd038fd63705e2101ba6d6
Víctor Andrés Rojas <victandres98@gmail.com>
2021-04-28 17:53:18 -0500

    Circle Image Component

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://www.publicdomainpictures.net/pictures/270000/velka/avatar-people-person-business-.jpg"),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text("VR"),
+              backgroundColor: Colors.purple,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage("assets/jar-loading.gif"),
+          image: NetworkImage(
+              "https://images.pexels.com/photos/6533790/pexels-photo-6533790.jpeg"),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
